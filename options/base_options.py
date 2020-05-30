7428f4c812a4da3701cf990afdb20a29127b37e4
Andrew Jong <andrewjong87@gmail.com>
2020-05-30 14:09:51 -0700

    enable switching between clean pose points or not
    by Motion Extraction Net

--- options/base_options.py
+++ options/base_options.py
@@ -9,7 +9,7 @@ import sys
 
 class BaseOptions():
     def __init__(self):
-        self.parser = argparse.ArgumentParser()
+        self.parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         self.initialized = False
 
     def initialize(self, parser):
